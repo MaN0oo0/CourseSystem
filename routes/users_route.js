6df028fe5e6627d1de8fa187ab6ad950e88bf77e
MaN0oo0 <orbanm316@gmail.com>
2023-11-28 17:04:33 +0200

    update v7

--- routes/users_route.js
+++ routes/users_route.js
@@ -46,5 +46,6 @@ router
   .route("/register")
   .post(upload.single("avatar"), usersController.register);
 router.route("/login").post(usersController.login);
+router.post(authToken);
 
 module.exports = router;
